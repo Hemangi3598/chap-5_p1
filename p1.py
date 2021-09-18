abea1377cc226cde41eed1b4d868d355745fd6fe
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-18 16:54:21 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,43 @@
+s1= "heMaNgi paTil"
+print(s1.upper())
+print(s1.lower())
+print(s1.swapcase())
+print(s1.title())
+print(s1.capitalize())
+
+
+s2= "Hemangi"
+print(s2.isalnum())  #t
+print(s2.isalpha())  #t
+print(s2.isdigit())	
+print(s2.islower())
+print(s2.isupper())
+print(s2.istitle())
+print(s2.isspace())
+
+s3 = "123"
+print(s3.isalnum())  #t
+print(s3.isalpha())  #t
+print(s3.isdigit())	
+print(s3.islower())
+print(s3.isupper())
+print(s3.istitle())
+print(s3.isspace())
+
+s4 = "  "
+print(s4.isalnum())  #t
+print(s4.isalpha())  #t
+print(s4.isdigit())	
+print(s4.islower())
+print(s4.isupper())
+print(s4.istitle())
+print(s4.isspace())
+
+
+#s5= "abc123"
+
+
+
+
+
+
